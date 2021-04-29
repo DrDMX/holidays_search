99ed66bfa4ea5b0af632abdee9e76a7477e2fa8e
Artem Neretin <artem@4vrs.com>
2021-04-29 20:06:31 +0700

    Add model and controler, setup migration for model

---
+++ database/migrations/2021_04_29_130405_create_holidays_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHolidaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('holidays', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->text('original_name');
+            $table->text('en_name');
+            $table->text('country_code');
+            $table->year('year');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('holidays');
+    }
+}
